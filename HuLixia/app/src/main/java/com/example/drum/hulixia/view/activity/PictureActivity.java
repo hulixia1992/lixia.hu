85ec4f9dfd0dc8c69983ef103ca701b4b4c05ff7
hulixia1992 <hulixia1992@gmail.com>
2016-08-01 17:54:01 +0800

    PictureActivity的ToolBar颜色随图片颜色改变

--- HuLixia/app/src/main/java/com/example/drum/hulixia/view/activity/PictureActivity.java
+++ HuLixia/app/src/main/java/com/example/drum/hulixia/view/activity/PictureActivity.java
@@ -1,20 +1,31 @@
 package com.example.drum.hulixia.view.activity;
 
-import android.media.Image;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.ViewCompat;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.graphics.Palette;
 import android.widget.ImageView;
 
 import com.example.drum.hulixia.R;
 import com.example.drum.hulixia.constants.Constants;
 import com.example.drum.hulixia.view.BaseActivity;
-import com.facebook.drawee.view.SimpleDraweeView;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class PictureActivity extends BaseActivity {
     private ImageView sdvPicture;
+    private String imageUrl;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //根据图片颜色更改ToolBar颜色
+            findViewById(R.id.toolbar).setBackgroundColor((Integer) msg.obj);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,32 @@ public class PictureActivity extends BaseActivity {
         initTitleView(getIntent().getStringExtra(Constants.IMAGE_DESC));
         sdvPicture = (ImageView) findViewById(R.id.sdv_picture_foods);
         ViewCompat.setTransitionName(sdvPicture, Constants.PICTURE);
-        String imageUrl = getIntent().getStringExtra(Constants.IMAGE_URL);
-        Log.i("hulixia", imageUrl);
+        imageUrl = getIntent().getStringExtra(Constants.IMAGE_URL);
         Picasso.with(this).load(imageUrl).into(sdvPicture);
-        // sdvPicture.setImageURI(Uri.parse(imageUrl));
+        initToolbarColor();
+
+    }
+
+    private void initToolbarColor() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = Picasso.with(PictureActivity.this).load(imageUrl).get();
+                    Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+                            Message msg = new Message();
+                            msg.obj = palette.getVibrantColor(getResources().getColor(R.color.main_color));
+                            handler.sendMessage(msg);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
