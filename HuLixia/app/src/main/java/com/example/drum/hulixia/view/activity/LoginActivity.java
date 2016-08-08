8cffeda5bbab394964623556bc114723c3328dfa
hulixia1992 <hulixia1992@gmail.com>
2016-08-08 13:37:24 +0800

    添加 MD风格的登录注册界面

---
+++ HuLixia/app/src/main/java/com/example/drum/hulixia/view/activity/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.drum.hulixia.view.activity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.drum.hulixia.R;
+
+public class LoginActivity extends AppCompatActivity {
+private FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        fab= (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+
+    private void initListener() {
+       fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options =
+                            ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, fab, fab.getTransitionName());
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class), options.toBundle());
+                } else {
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                }
+            }
+        });
+    }
+}
+
